270059019e4a838f231fa0ec1a446783192f9d17
HamsterTime <travistancookie@gmail.com>
2026-08-31 17:20:50 +0800

    Initial Commit

---
+++ Final Repo Please/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Final Repo Please
+//
+//  Created by Travis on 31/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
