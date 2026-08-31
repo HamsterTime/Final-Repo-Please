c1db95568368dde702bacbb23a60c37ddb631486
HamsterTime <travistancookie@gmail.com>
2026-08-31 17:20:50 +0800

    Initial Commit

---
+++ Final Repo Please/Final_Repo_PleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Repo_PleaseApp.swift
+//  Final Repo Please
+//
+//  Created by Travis on 31/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Final_Repo_PleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
